8391e631b8b98b52a559e5c0ac4aa31e77af51cd
mohdshanin <shansid2000@gmail.com>
2021-02-19 22:02:34 +0530

    change

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -8,16 +8,18 @@ const UserItem = () => {
         html_url: "https://github.com/mojombo",
       }
     
+      const {login, avatar_url, html_url} = this.state
     return (
+
         <div className='card text-center' >
             <img 
-              src={this.state.avatar_url} 
+              src={avatar_url} 
               alt='' className='round-img' 
               style={{width: '60px'}} 
             />
-            <h3>{this.state.login} </h3>
+            <h3>{login} </h3>
             <div>
-                <a href="this.state.html_url" className="btn btn-dark btn-sm my-1">More</a>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
         </div>
     )
